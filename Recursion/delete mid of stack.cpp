57214266560d6187ed380847e023e85b2cfdf64d
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-01 21:45:37 +0530

    Create delete mid of stack.cpp

---
+++ Recursion/delete mid of stack.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void del_mid(stack<int>&s,int size,int original_size)
+{
+    if(size==(original_size/2)+1)
+    {
+        s.pop();
+        return;
+    }
+    
+    int val=s.top();
+    s.pop();
+    
+    del_mid(s,--size,original_size);
+    s.push(val);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    stack<int>s;
+    
+    for(int i=0;i<n;i++)
+    {
+        int h;
+        cin>>h;
+        
+        s.push(h);
+    }
+  //Here we are passing size and original_size
+  //we can directy pass mid value only and decrease it and create our base condition accordingly 
+    del_mid(s,n,n);
+
+    while(!s.empty())
+    {
+        cout<<s.top();
+        s.pop();
+    }
+    
+    return 0;
+}
