c14f3783f48beeb62b25d58779b9e783f333ed42
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-03 00:43:13 +0530

    Create addPermutation.cpp

---
+++ Recursion/addPermutation.cpp
@@ -0,0 +1,42 @@
+//We need to add permutation inside string
+// Eg. for ABC
+// A_B_C A_BC AB_C ABC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void addPermutation(string ip,string op)
+{
+ 
+    if(ip.length()==0)
+    {
+        cout<<"\""<<op<<"\",";
+        return;
+    }
+    
+   string op1=op,op2=op;
+   
+   op2=op2+"_"+ip[0];
+   op1=op1+ip[0];
+   ip.erase(ip.begin()+0);
+    
+   addPermutation(ip,op1);
+   addPermutation(ip,op2);
+    
+}
+
+int main()
+{
+    string input,output;
+    
+    cin>>input;
+ 
+    output+=input[0];
+    input.erase(input.begin()+0);   
+   
+    cout<<"Subsets of given string are : \n ";
+    
+    addPermutation(input,output);
+    
+    return 0;
+}
