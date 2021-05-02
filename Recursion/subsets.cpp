1e62254820e31f786ef022ca4163f3e3bf7ee9ad
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-03 00:05:21 +0530

    Create subsets.cpp

---
+++ Recursion/subsets.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void subset(string ip,string op)
+{
+    
+    if(ip.length()==0)
+    {
+        cout<<"\""<<op<<"\",";
+        return;
+    }
+    
+   string op1=op,op2=op;
+   
+   op2+=ip[0];
+   
+   ip.erase(ip.begin()+0);
+   
+   subset(ip,op1);
+   subset(ip,op2);
+   
+    
+}
+
+int main()
+{
+    string input,output;
+    
+    cin>>input;
+    
+    cout<<"Subsets of given string are : \n ";
+    
+    subset(input,output);
+    
+    return 0;
+}
