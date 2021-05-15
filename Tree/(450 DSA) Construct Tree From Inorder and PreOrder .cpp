478d5a2f3ed50fff5bd066e2b97affd75f68ab39
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-15 21:10:10 +0530

    Create (450 DSA) Construct Tree From Inorder and PreOrder .cpp

---
+++ Tree/(450 DSA) Construct Tree From Inorder and PreOrder .cpp
@@ -0,0 +1,27 @@
+// l is 0 h is length of array and preIndex is 0
+
+Node*construct(int in[],int pre[],int l,int h,int&preIndex)
+    {
+   
+            if(l > h)
+        return NULL;
+        
+    Node* root = new Node(pre[preIndex++]);
+    
+    if(l == h)
+        return root;
+        
+    int i;
+    for(i=l;i<=h;i++)
+        if(root->data == in[i])
+            break;
+            
+    if(i <= h)
+    {
+        root->left = construct(in, pre, l, i-1,preIndex);
+        
+        root->right = construct(in, pre, i+1, h,preIndex);
+    }
+    
+    return root;
+    }
