6071c0ea0ec175d22f9d6fab68250850ddbb3a6e
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-21 00:03:14 +0530

    Create (450 DSA)Isomorphic Trees.cpp

---
+++ Tree/(450 DSA)Isomorphic Trees.cpp
@@ -0,0 +1,19 @@
+//Only nodes of left and right can be swapped 
+
+  bool isIsomorphic(Node *root1,Node *root2)
+    {
+    //add code here.
+    
+    if(!root1&&!root2)
+        return true;
+            if(!root1||!root2)
+        return false;
+    if(root1->data!=root2->data)    
+        return false;
+    
+    return (isIsomorphic(root1->left,root2->left)&&
+    isIsomorphic(root1->right,root2->right)||
+    isIsomorphic(root1->left,root2->right)&&
+    isIsomorphic(root1->right,root2->left));
+
+    }
