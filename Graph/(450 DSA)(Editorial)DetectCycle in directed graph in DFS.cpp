5f220ba18e6a0f124e1178129a5719762c45d77d
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-27 21:59:08 +0530

    Create (450 DSA)(Editorial)DetectCycle in directed graph in DFS.cpp

---
+++ Graph/(450 DSA)(Editorial)DetectCycle in directed graph in DFS.cpp
@@ -0,0 +1,52 @@
+//First we are making every node unvisited 
+//One by one add node to stack and make it visited
+//Now do if its adj. node not visited keep doing ame thing for it as well
+//if a node if visited and also in stack ,that means cycle if formed
+
+//if not then rmeove it from stack when it is not conncted further or all adj. nodes are visited
+
+bool Graph::isCyclicUtil(int v, bool visited[], bool *recStack)
+{
+    if(visited[v] == false)
+    {
+        // Mark the current node as visited and part of recursion stack
+        visited[v] = true;
+        recStack[v] = true;
+ 
+        // Recur for all the vertices adjacent to this vertex
+        list<int>::iterator i;
+        for(i = adj[v].begin(); i != adj[v].end(); ++i)
+        {
+            if ( !visited[*i] && isCyclicUtil(*i, visited, recStack) )
+                return true;
+            else if (recStack[*i])
+                return true;
+        }
+ 
+    }
+    recStack[v] = false;  // remove the vertex from recursion stack
+    return false;
+}
+ 
+// Returns true if the graph contains a cycle, else false.
+// This function is a variation of DFS() in https://www.geeksforgeeks.org/archives/18212
+bool Graph::isCyclic()
+{
+    // Mark all the vertices as not visited and not part of recursion
+    // stack
+    bool *visited = new bool[V];
+    bool *recStack = new bool[V];
+    for(int i = 0; i < V; i++)
+    {
+        visited[i] = false;
+        recStack[i] = false;
+    }
+ 
+    // Call the recursive helper function to detect cycle in different
+    // DFS trees
+    for(int i = 0; i < V; i++)
+        if (isCyclicUtil(i, visited, recStack))
+            return true;
+ 
+    return false;
+}
