8309d043f0efdb86a0fcd773efafa8f5cec0a175
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-27 20:07:09 +0530

    Update (450 DSA)(Editorial)DFS.cpp

--- Graph/(450 DSA)(Editorial)DFS.cpp
+++ Graph/(450 DSA)(Editorial)DFS.cpp
@@ -3,6 +3,15 @@
 
 //first print current node and add its list and for each element of list repeat the same. 
 
+// for checking visited or not:
+//     map<int, bool> visited;
+
+// Graph adj. map:
+//     map<int, list<int>> adj;
+
+// DFS traversal of the vertices reachable from v
+    void DFS(int v);
+
 void Graph::DFS(int v)
 {
     // Mark the current node as visited and
