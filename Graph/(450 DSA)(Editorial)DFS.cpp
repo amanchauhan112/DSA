656b5c49f3e095450d2c3540e91f6e13ff39c167
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-27 20:00:43 +0530

    Create (450 DSA)(Editorial)DFS.cpp

---
+++ Graph/(450 DSA)(Editorial)DFS.cpp
@@ -0,0 +1,19 @@
+// Recursive function for DFS:
+// where v is the starting node/vertex
+
+//first print current node and add its list and for each element of list repeat the same. 
+
+void Graph::DFS(int v)
+{
+    // Mark the current node as visited and
+    // print it
+    visited[v] = true;
+    cout << v << " ";
+ 
+    // Recur for all the vertices adjacent
+    // to this vertex
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFS(*i);
+}
