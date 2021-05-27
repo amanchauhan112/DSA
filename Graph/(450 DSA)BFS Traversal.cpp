51ff3303e3bf981a3be771729ebb726b8017349b
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-27 15:31:34 +0530

    Update (450 DSA)BFS Traversal.cpp

--- Graph/(450 DSA)BFS Traversal.cpp
+++ Graph/(450 DSA)BFS Traversal.cpp
@@ -3,13 +3,7 @@
 	    // Code here
 	    
 	    //For understanding purpose
-	    // First node connected is given in adj
-	    //For eg. : if 0 connected to 1 2 3
-	    //adj[0] : 0->1->2->3
-	   // then 1 and 3 are leaf node
-	   // Node 2 is connected to leaf 4 
-	   // so adj[2]: 2->4
-	   // and adj[1],3,4:1,3,4
+
 	    
 	    vector<int>v;
 	    vector<int>visited(V,0);
