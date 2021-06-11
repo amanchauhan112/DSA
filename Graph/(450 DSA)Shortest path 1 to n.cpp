c1b08cb41b407fc831cbdf7babff4d94968f8076
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-06-11 22:59:51 +0530

    Create (450 DSA)Shortest path 1 to n.cpp

---
+++ Graph/(450 DSA)Shortest path 1 to n.cpp
@@ -0,0 +1,19 @@
+ int minimumStep(int n){
+        //complete the function here
+        
+        int c=0;
+        while(n>1)
+        {
+            if(n%3==0)
+                {
+                    n=n/3;
+                }
+            
+            else{
+                n--;
+            }
+            c++;
+        }
+        
+        return c;
+    }
